aee912b6cb5e46261edc1d76e653f21e60fe6b5a
Weslley Coelho <wes.coelho18@gmail.com>
2022-11-01 01:30:15 -0300

    first commit

---
+++ mobile/App.tsx
@@ -0,0 +1,29 @@
+import { NativeBaseProvider, Box, Center, StatusBar } from "native-base";
+import { THEME } from "./src/styles/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { Loading } from "./src/components/Loading";
+import { SignIn } from "./src/screens/Signin";
+
+export default function App() {
+  const [fontLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={"transparent"}
+        translucent
+      />
+      {fontLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
